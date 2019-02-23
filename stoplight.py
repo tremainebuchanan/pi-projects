ebb9f978ad9dc7addd8cd34e7a2a216a8b8f01b5
Tremaine Buchanan <tremainekbuchanan@gmail.com>
2019-02-23 08:44:51 -0500

    added comments

--- stoplight.py
+++ stoplight.py
@@ -1,28 +1,32 @@
+'Stop Light program using Raspberry Pi'
+'Author: Tremaine Buchanan'
 import RPi.GPIO as GPIO
 import time
 
-
-led_red = 14
-led_yellow = 15
-led_green = 18
+led_red = 14 # Assign red LED to GPIO 14
+led_yellow = 15 # Assign yellow LED to GPIO 15 
+led_green = 18 # Assign green LED to GPIO 18
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(led_red, GPIO.OUT)
 GPIO.setup(led_yellow, GPIO.OUT)
 GPIO.setup(led_green, GPIO.OUT)
 
-def lights_off(led_red, led_yellow, led_green):
+'Ensures the stoplight starts at red once turned on'
+def reset(led_red, led_yellow, led_green):
     GPIO.output(led_red, False)
     GPIO.output(led_yellow, False)
     GPIO.output(led_green, False)
-    
+
+'Turns on a specific LED'
 def light_on(value):
     GPIO.output(value, True)
-
+    
+'Turns off a specific LED'
 def light_off(value):
     GPIO.output(value, False)
     
-lights_off(led_red, led_yellow, led_green)
+reset(led_red, led_yellow, led_green)
 
 while True:
     light_on(led_red)
